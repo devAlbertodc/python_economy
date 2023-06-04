80551a5929022d93ec891d8bc85b3df27e915a91
Alberto <aldacala@hotmail.es>
2023-06-04 18:47:29 +0200

    Add documentation to the files and delete test files

--- algorithmic_trading_quantitative_analysis/02_yahoofin_intro.py
+++ algorithmic_trading_quantitative_analysis/02_yahoofin_intro.py
@@ -1,12 +1,6 @@
-# -*- coding: utf-8 -*-
-"""
-yahoofin library intro
-
-@author: Mayank Rasu (http://rasuquant.com/wp/)
-"""
-
 from yahoofinancials import YahooFinancials
 
+#Get ticker info using YahooFinancials class to create an objeect and use its methods:
 ticker = 'MSFT'
 yahoo_financials = YahooFinancials(ticker)
 data = yahoo_financials.get_historical_price_data("2018-04-24", "2020-04-24", "daily")
