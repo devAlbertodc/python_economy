e0bf0c21150da90f62fa7c3fccc1c6332a7c66e5
devAlbertodc <aldacala@hotmail.es>
2023-06-17 23:38:10 +0200

    Organize scripts into diff folders

--- algorithmic_trading_quantitative_analysis/yahoo_finance_and_technical_indicators/02_yahoofin_intro.py
+++
@@ -1,6 +0,0 @@
-from yahoofinancials import YahooFinancials
-
-#Get ticker info using YahooFinancials class to create an objeect and use its methods:
-ticker = 'MSFT'
-yahoo_financials = YahooFinancials(ticker)
-data = yahoo_financials.get_historical_price_data("2018-04-24", "2020-04-24", "daily")
